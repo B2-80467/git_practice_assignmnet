49602dafd935ccfcada72b2934aadb20e49b609b
sandy shinde <samirshinde011@gmail.com>
2022-11-02 12:01:33 +0530

    writen prime code

---
+++ prime.js
@@ -0,0 +1,18 @@
+function primechecker(number){
+
+  let factors=0;
+for(i=1;i<number; i++){
+  if(number%i==0){
+          factor
+  }
+if(factor==2){
+     
+   return=true
+}
+   return=false
+}
+
+
+}  
+let  ans=primechecker(13)
+    (ans==true)?console.log(ans,"is prime"):consle.log(ans, "is not prime");
